2ba6533eddfa2f0313aae74b12f4ea3bcd912196
Luke Dekan <lukedekan@gmail.com>
2024-06-19 14:56:40 -0500

    Start of Aiutomatic PNG Move

---
+++ png_move.py
@@ -0,0 +1,12 @@
+import os 
+import shutil 
+
+
+# Move file function 
+def move_file():
+    
+    # Set the source location use *.png for extension using shutil directory 
+
+    # Set destination location 
+
+    # Move file to location 
